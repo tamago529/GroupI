27d34c1e4d3d428a6dabf2853df62a7620c2c4a2
tamago529 <tamago29291@gmail.com>
2025-11-25 15:04:49 +0900

    インストールアップ追加

--- tabettiproject/tabettiproject/settings.py
+++ tabettiproject/tabettiproject/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'accounts.apps.AccountsConfig',
+    'follow.apps.FollowConfig',
+    'reservation.apps.ReservationConfig',
+    'reservation_management.apps.ReservationManagementConfig',
+    'review.apps.ReviewConfig',
+    'store.apps.StoreConfig',
+    'search.apps.SearchConfig',   
 ]
 
 MIDDLEWARE = [
@@ -103,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
