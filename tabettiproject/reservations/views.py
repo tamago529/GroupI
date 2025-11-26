b865ce5bd5e32c2cfcd307571cc8ea8654122a8c
gitmr8892 <gitmr8892@gmail.com>
2025-11-26 12:23:14 +0900

    hennko

--- tabettiproject/reservations/views.py
+++ tabettiproject/reservations/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+
+class 	reservation_registerView(TemplateView):
+    template_name = "reservation_register.html"
+
+class 	reservation_confirmView(TemplateView):
+    template_name = "reservation_confirm.html"
+
+class 	reservation_editView(TemplateView):
+    template_name = "reservation_edit.html"
+
+class 	reservation_cancelView(TemplateView):
+    template_name = "reservation_cancel.html"
+
+class 	reservation_historyView(TemplateView):
+    template_name = "reservation_history.html"
+
--- tabettiproject/reviews/views.py
+++ tabettiproject/reviews/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views import TemplateView
 
-# Create your views here.
+class review_list_customerView(TemplateView):
+    template_name = "review_list_customer.html"
+
+class review_list_reviewerView(TemplateView):
+    template_name = "review_list_reviewer.html"
+
+class reviewer_detailView(TemplateView):
+    template_name = "reviewer_detail.html"
+
+class reviewer_searchView(TemplateView):
+    template_name = "reviewer_search.html"
+
+class review_report_customerView(TemplateView):
+    template_name = "review_report_customer.html"
+
+class review_report_storeView(TemplateView):
+    template_name = "review_report_store.html"
+
+class review_list_storeView(TemplateView):
+    template_name = "review_list_store.html"
+
+class review_list_companyView(TemplateView):
+    template_name = "review_list_company.html"
+
+class customer_report_input(TemplateView):
+    template_name = "customer_report_input.html"
+
