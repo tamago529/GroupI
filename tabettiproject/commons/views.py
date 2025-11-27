70c0981fdd69525b1b7265ac3c9aedd9faa7ff8d
JP_blank <Originaluser.blank@gmail.com>
2025-11-27 12:28:05 +0900

    delete

--- tabettiproject/commons/views.py
+++ tabettiproject/commons/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class customer_common_completeView(TemplateView):
-    template_name = 'customer_commmon_complete.html'
+    template_name = 'customer_common_complete.html'
 
 class customer_common_confirmView(TemplateView):
-    template_name = 'customer_commmon_confirm.html'
+    template_name = 'customer_common_confirm.html'
 
 class errorView(TemplateView):
     template_name = 'error.html'
