b8aec2d070789ec7a61ec965e744f051c774a3c1
reo-ja <leochi0213@gmail.com>
2025-11-27 14:19:41 +0900

    なんかエラー起きました

--- tabettiproject/tabettiproject/settings.py
+++ tabettiproject/tabettiproject/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'tabettiproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
