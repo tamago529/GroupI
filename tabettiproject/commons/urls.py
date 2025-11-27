3e15cb0b86d234731f96bed7643936337765ff65
reo-ja <leochi0213@gmail.com>
2025-11-27 14:33:39 +0900

    accounts,commonsのパス変更

--- tabettiproject/commons/urls.py
+++ tabettiproject/commons/urls.py
@@ -4,10 +4,10 @@ from .views import *
 app_name = "common"
 
 urlpatterns = [
-  path("customer_common_complete/", customer_common_completeView.as_view(), name="customer_common_complete.html"),
-  path("customer_common_confirm/", customer_common_confirmView.as_view(), name="customer_common_confirm.html"),
-  path("error/", errorView.as_view(), name="error.html"),
-  path("store_common_complete/", store_common_completeView.as_view(), name='store_common_complete.html'),
-  path("store_common_confirm/", store_common_confirmView.as_view(), name='store_common_confirm.html')
+  path("commons/customer_common_complete/", customer_common_completeView.as_view(), name="customer_common_complete.html"),
+  path("commons/customer_common_confirm/", customer_common_confirmView.as_view(), name="customer_common_confirm.html"),
+  path("commons/error/", errorView.as_view(), name="error.html"),
+  path("commons/store_common_complete/", store_common_completeView.as_view(), name='store_common_complete.html'),
+  path("commons/store_common_confirm/", store_common_confirmView.as_view(), name='store_common_confirm.html')
 
 ]
