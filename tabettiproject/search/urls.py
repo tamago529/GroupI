1e5cb6b51405a6e2d2c6006c0b7daa45b8a8ed7f
tamago529 <tamago29291@gmail.com>
2025-11-28 10:07:48 +0900

    ff

--- tabettiproject/search/urls.py
+++ tabettiproject/search/urls.py
@@ -4,8 +4,8 @@ from .views import *
 app_name = "search"
 
 urlpatterns = [
-  path("search/top/", customer_topView.as_view(), name="customer_top.html"),
-  path("search/genre_list/", customer_genre_listView.as_view(), name="customer_genre_list.html"),
-  path("search/search_list/", customer_search_listView.as_view(), name="customer_search_list.html")
+  path("top/", customer_topView.as_view(), name="customer_top.html"),
+  path("genre_list/", customer_genre_listView.as_view(), name="customer_genre_list.html"),
+  path("search_list/", customer_search_listView.as_view(), name="customer_search_list.html")
 
 ]
