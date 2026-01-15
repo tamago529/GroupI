e12422587a19ef0628d2163417d3f46826171e12
chikuwa-1116 <toyachikikun@gmail.com>
2026-01-15 12:02:03 +0900

    reviewsのcompanyの部分とsettingいじりました

--- tabettiproject/reviews/views.py
+++ tabettiproject/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from commons.models import Review, ReviewReport
 
 class customer_review_listView(TemplateView):
     template_name = "reviews/customer_review_list.html"
@@ -32,6 +33,18 @@ class store_review_listView(TemplateView):
 
 class company_review_listView(TemplateView):
     template_name = "reviews/company_review_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ① すべての口コミを取得（投稿が新しい順）
+        # select_relatedを使うと、投稿者(reviewer)や店舗(store)のデータも一緒に取れるので動作が速くなります
+        reviews = Review.objects.select_related('reviewer', 'store').all().order_by('-posted_at')
+        
+        # ② 「どの口コミが通報されているか」のIDリストを取得（チェックボックス用）
+        reported_review_ids = ReviewReport.objects.values_list('review_id', flat=True).distinct()
+        
+        context['reviews'] = reviews
+        context['reported_review_ids'] = reported_review_ids
+        return context
 
 
 class customer_report_input(TemplateView):
@@ -39,3 +52,11 @@ class customer_report_input(TemplateView):
 
 class reportView(TemplateView):
     template_name = "reviews/report.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ① すべての通報（Report）を取得
+        # 通報者(reporter)と、通報された口コミ(review)のデータをまとめて取得
+        reports = ReviewReport.objects.select_related('review', 'reporter', 'review__reviewer').all().order_by('-reported_at')
+        
+        context['reports'] = reports
+        return context
--- tabettiproject/tabettiproject/settings.py
+++ tabettiproject/tabettiproject/settings.py
@@ -143,3 +143,4 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR ,'static'),  # 
 ]
 
+AUTH_USER_MODEL = 'commons.Account'
