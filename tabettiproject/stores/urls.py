afda33cc433e4aa1084cd4a4f8154fab402ef0ee
chikuwa-1116 <toyachikikun@gmail.com>
2026-01-16 12:29:48 +0900

    storesのcompanystoremanagementとurlsとviewsやったよん

--- tabettiproject/stores/urls.py
+++ tabettiproject/stores/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("customer_menu_course/", customer_menu_courseView.as_view(), name="customer_menu_course"),
     path("customer_store_basic_edit/", customer_store_basic_editView.as_view(), name="customer_store_basic_edit"),
     path("store_basic_edit/", store_basic_editView.as_view(), name="store_basic_edit"),
-    path("company_store_info/", company_store_infoView.as_view(), name="company_store_info"),
+    path("company_store_info/<int:pk>", company_store_infoView.as_view(), name="company_store_info"),
     path("customer_store_info/", customer_store_infoView.as_view(), name="customer_store_info"),
     path("customer_store_new_register/", customer_store_new_registerView.as_view(), name="customer_store_new_register"),
     path("customer_store_new_register_confirm/", customer_store_new_register_confirmView.as_view(), name="customer_store_new_register_confirm"),
@@ -17,5 +17,6 @@ urlpatterns = [
     path("store_logout/", store_logoutView.as_view(), name="store_logout"),
     path("company_store_management/", company_store_managementView.as_view(), name="company_store_management",
     ),
+    path("company_store_delete_execute/<int:pk>/", store_delete_execute, name="store_delete_execute"),
 
 ]
--- tabettiproject/stores/views.py
+++ tabettiproject/stores/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 from django.views.generic.base import TemplateView
-from commons.models import StoreAccount
+from commons.models import StoreAccount, Store
+from django.urls import reverse # 追記
+from django.db.models import Q # 追記
+import urllib.parse # 追記
+
+
 
 class customer_mapView(TemplateView):
     template_name = "stores/customer_map.html"
@@ -21,7 +26,11 @@ class store_basic_editView(TemplateView):
 
 class company_store_infoView(TemplateView):
     template_name = "stores/company_store_info.html"
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # URLから渡されたID(pk)を使って、特定の店舗1件を取得
+        context['store'] = get_object_or_404(Store, pk=self.kwargs['pk'])
+        return context
 
 class customer_store_infoView(TemplateView):
     template_name = "stores/customer_store_info.html"
@@ -57,3 +66,28 @@ class store_logoutView(TemplateView):
 
 class company_store_managementView(TemplateView):
     template_name = "stores/company_store_management.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # 検索キーワードを取得
+        query = self.request.GET.get('q', '')
+        
+        if query:
+            # 店舗名にキーワードを含むものを検索
+            stores = Store.objects.filter(Q(store_name__icontains=query)).order_by('store_name')
+        else:
+            # キーワードがなければ全件
+            stores = Store.objects.all().order_by('store_name')
+            
+        context['stores'] = stores
+        context['query'] = query
+        return context
+def store_delete_execute(request, pk):
+    # 削除対象の店舗を取得
+    store = get_object_or_404(Store, pk=pk)
+    name = store.store_name
+    store.delete() # 削除実行
+    
+    # 完了画面へメッセージ付きでリダイレクト
+    msg = f"店舗「{name}」の削除"
+    encoded_msg = urllib.parse.quote(msg)
+    return redirect(reverse('commons:company_common_complete') + f"?message={encoded_msg}")
