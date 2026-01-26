69366c658dd1965febd7612ea554e93248a66ace
reo-ja <leochi0213@gmail.com>
2026-01-26 10:04:27 +0900

    パスワードリセットしたあとログインできるようになったよん

--- tabettiproject/accounts/forms.py
+++ tabettiproject/accounts/forms.py
@@ -4,28 +4,27 @@ from django.contrib.auth import authenticate
 from commons.models import Account
 
 class CustomerLoginForm(AuthenticationForm):
-    # HTMLの name="username" にあたる部分を「メールアドレス」として定義
-    username = forms.EmailField(label="メールアドレス")
+    # HTMLの name="username" にあたる部分を「ユーザーネーム」として定義
+    username = forms.CharField(label="ユーザーネーム")
 
     def clean(self):
-        email = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
+        cleaned_data = super().clean()  # AuthenticationFormの基本バリデーションも通す
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
 
-        if email and password:
-            # データベースからメールアドレスでユーザーを探して認証
-            try:
-                user_obj = Account.objects.get(email=email)
-                # パスワードの照合
-                self.user_cache = authenticate(self.request, username=user_obj.username, password=password)
-            except Account.DoesNotExist:
-                self.user_cache = None
+        if username and password:
+            # ✅ ユーザーネームで認証する
+            self.user_cache = authenticate(
+                self.request,
+                username=username,
+                password=password
+            )
 
             if self.user_cache is None:
                 raise self.get_invalid_login_error()
-            else:
-                self.confirm_login_allowed(self.user_cache)
-        return self.cleaned_data
+            self.confirm_login_allowed(self.user_cache)
 
+        return cleaned_data
 
 class CustomerRegisterForm(forms.ModelForm):
     # パスワード確認用フィールド
