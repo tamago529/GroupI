b08e2b3e69eefd65bf29ba981cc68fda1568f520
tamago529 <tamago29291@gmail.com>
2026-02-06 14:19:22 +0900

    Revert "deploy"
    This reverts commit 4bfda8a1566a91fc09e7b6709f10c61475fe6f27.

--- tabettiproject/tabettiproject/settings.py
+++ tabettiproject/tabettiproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,14 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_p0ohz^33d#g&v98-s*u9$*o6+@^23z*8@)%2pt(erz&-$@*^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['your-domain.com', 'サーバーIP']
+ALLOWED_HOSTS = []
 
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
-SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
 
 # Application definition
 
@@ -91,11 +86,11 @@ WSGI_APPLICATION = 'tabettiproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 
 # Password validation
@@ -168,5 +163,3 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = DEFAULT_FROM_EMAIL  # 念のため（エラー通知など）
 
 LOGIN_URL = "accounts:customer_login"
-
-
